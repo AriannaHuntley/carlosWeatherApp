1629fe39cbfc0eb9873234e16462132d9e3d5e58
arianna huntley <your_email@example.com>
2025-12-18 16:52:34 -0800

    2nd

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,29 @@
-const UserSearchInput = document.getElementById("UserSearchInput");
+import { APIKEY } from './environment.js';
+
+navigator.geolocation.getCurrentPosition(success, errorFunc);
+
+// const UserSearchInput = document.getElementById("UserSearchInput");
+
+function success(position){
+    console.log(position);
+    console.log("Our latitude: " + position.coords.latitude);
+    console.log("Our longitude: " + position.coords.longitude);
+    console.log("We know where you are!");
+
+}
+
+function errorFunc(error){
+    console.log(error.message);
+}
+
+function apiCall () {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${ APIKEY }`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+}
+
+apiCall();
